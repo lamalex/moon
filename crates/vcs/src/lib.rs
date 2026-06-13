08fb8a973565426efae982db6ceba30593d03de8
Alex Launi <alauni@actblue.com>
2026-06-13 15:19:10 -0400

    feat(vcs): add workspace file services

--- crates/vcs/src/lib.rs
+++ crates/vcs/src/lib.rs
@@ -2,8 +2,10 @@ pub mod git;
 
 mod changed_files;
 mod vcs;
+mod workspace_files;
 
 pub use changed_files::*;
 pub use vcs::*;
+pub use workspace_files::*;
 
 pub type BoxedVcs = Box<dyn Vcs + Send + Sync + 'static>;
